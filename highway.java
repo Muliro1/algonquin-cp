9fa94e4948aa88a934434f0585dada65ed965734
Muliro1 <mulirokhaemba@gmail.com>
2026-02-12 15:49:23 -0500

    edits

---
+++ highway.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+      int highwayNumber;
+      int primaryNumber;
+
+      highwayNumber = scnr.nextInt();
+
+      if (highwayNumber % 100 == 0) {
+            System.out.println(highwayNumber + " is not a valid interstate highway number.");
+      }
+       
+      else if ((highwayNumber < 100) && (highwayNumber % 2 == 0)){
+         System.out.println("I-" + highwayNumber + " is primary, going east/west." );
+      }
+      else if ((highwayNumber % 2 != 0) && (highwayNumber < 100)){
+         System.out.println("I-" + highwayNumber + " is primary, going north/south.");
+         }
+      else if (highwayNumber > 100){
+          String strNumber = String.valueOf(highwayNumber);
+          String subStringResult = strNumber.substring(1);
+          int subIntResult = Integer.parseInt(subStringResult);
+          //System.out.println(subIntResult);
+          if (subIntResult % 2 == 0){
+            System.out.println("I-" + highwayNumber + " is auxiliary, serving " + "I-" + subStringResult + ", going east/west.");
+          }
+          else {
+            System.out.println("I-" + highwayNumber + " is auxiliary, serving " + "I-" + subStringResult + ", going north/south.");
+          }
+      }
+    }
+}
